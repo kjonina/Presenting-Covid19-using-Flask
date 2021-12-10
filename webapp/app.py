e101a95c346e6995414532215ac13cbc938aa84c
kjonina <kjonina@gmail.com>
2021-12-10 20:05:40 +0000

    Flask up and loaded

--- webapp/app.py
+++ webapp/app.py
@@ -3,24 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
 @app.get("/")
-def hello():
-    return "Hello from my first Flask webapp with debugging."
-
-
-@app.get("/page1")
 def show_page1():
-    return render_template("page1.html", title="This is page 1")
-
-
-@app.get("/page2")
-def show_page2():
-    return render_template("page2.html", title="This is page 2")
-
-
-@app.get("/page3")
-def show_page3():
-    return render_template("page3.html", title="This is page 3")
+    return render_template("home_page.html")
 
 
 if __name__ == "__main__":  # Will eval to False when this code imported.
